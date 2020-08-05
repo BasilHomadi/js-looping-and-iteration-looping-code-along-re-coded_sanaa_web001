dff97b210519b722bc38a78963d315ffa08c69c6
Basil Homadi <deviltearbasil@gmail.com>
2020-08-05 16:22:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 Function writeCards(nameList, eventName) {
   let cards = []
   for(let i = 0; i < nameList.length; i++) {
-    cards.push("Thank you, ${nameList[i}")
+    cards.push("Thank you, ${nameList[i}, for the wonderful ${eventName} gift!")
   }
+  return cards
 }
